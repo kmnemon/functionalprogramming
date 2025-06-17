15f1e8721e588aaaf46dba54bf2c7915b8efe839
kmnemon <kmnemon@outlook.com>
2025-06-17 13:28:21 +0700

    free ship

--- FP/FP/MegaMart-2/FirstMM.swift
+++ FP/FP/MegaMart-2/FirstMM.swift
@@ -16,9 +16,16 @@ struct ShoppingListItem: Hashable {
     let price: Double
 }
 
+let shoppingList: [ShoppingListItem] = [
+    ShoppingListItem(name: "Apples", price: 1.99),
+    ShoppingListItem(name: "Milk", price: 8.49),
+    ShoppingListItem(name: "Bread", price: 4.79)
+]
+
 //MVVM
 var shoppingCart: [ShoppingCartItem] = []
 var shoppingCartTotalStore: Double = 0
+var showFreeShippingsStore: [String: Bool] = [:]
 
 func addItemToCart(_ name: String, _ price: Double) {
     shoppingCart.append(ShoppingCartItem(name: name, price: price))
@@ -28,5 +35,12 @@ func addItemToCart(_ name: String, _ price: Double) {
 func calcCartTotal() {
     shoppingCartTotalStore = shoppingCart.reduce(0) { result, item in
         result + item.price
+    }
+    updateShipIcons()
+}
+
+func updateShipIcons() {
+    for shoppingItem in shoppingList {
+        showFreeShippingsStore[shoppingItem.name] = (shoppingCartTotalStore + shoppingItem.price) >= 20
     }
 }
--- FP/FP/MegaMart-2/MegaMartView.swift
+++ FP/FP/MegaMart-2/MegaMartView.swift
@@ -10,12 +10,6 @@ import SwiftUI
 struct MegaMartView: View {
     @State var shoppingCartTotal: Double = 0
     
-    let shoppingList: [ShoppingListItem] = [
-        ShoppingListItem(name: "Apples", price: 1.99),
-        ShoppingListItem(name: "Milk", price: 2.49),
-        ShoppingListItem(name: "Bread", price: 1.79)
-    ]
-    
     var body: some View {
         List {
             ForEach(shoppingList, id: \.self) { item in
@@ -24,6 +18,9 @@ struct MegaMartView: View {
                     Button("Buy") {
                         addItemToCart(item.name, item.price)
                         shoppingCartTotal = shoppingCartTotalStore
+                    }
+                    if showFreeShippingsStore[item.name] ?? false {
+                        Text("Free Shipping")
                     }
                 }
             }
