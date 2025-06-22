c39bf32f43d0b0f989ab1b319c032a0ec05af975
kmnemon <kmnemon@outlook.com>
2025-06-22 09:36:55 +0700

    array fp

---
+++ FP/FP/Utility/Array.swift
@@ -0,0 +1,43 @@
+//
+//  Array.swift
+//  FP
+//
+//  Created by ke Liu on 6/22/25.
+//
+
+
+func firstElement<T>(_ array: [T]) -> T? {
+   return array.first
+}
+
+func dropFirst<T>(_ array: [T]) -> [T] {
+    return Array(array.dropFirst())
+}
+
+func shift<T>(_ array: [T]) -> (T?, [T]) {
+    return (array.first, dropFirst(array))
+}
+
+func lastElement<T>(_ array: [T]) -> T? {
+    return array.last
+}
+
+func dropLast<T>(_ array: [T]) -> [T] {
+    return Array(array.dropLast())
+}
+
+func popLast<T>(_ array: [T]) -> (T?, [T]) {
+    return (array.last, dropLast(array))
+}
+
+func push<T>(_ array: [T], _ element: T) -> [T] {
+    var newArray = array
+    newArray.append(element)
+    return newArray
+}
+
+func arraySet<T>(_ array: [T], idx: Int, value: T) -> [T] {
+    var newArray = array
+    newArray[idx] = value
+    return newArray
+}
