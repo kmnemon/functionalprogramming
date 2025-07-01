f4d8685585957d31cb00fbf4e1239dc02dd5d76e
kmnemon <kmnemon@outlook.com>
2025-07-02 06:51:11 +0700

    d

--- FP/FP/Utility/Copy.swift
+++ FP/FP/Utility/Copy.swift
@@ -5,8 +5,51 @@
 //  Created by ke Liu on 6/22/25.
 //
 
+protocol DeepCopyable {
+    func deepCopy() -> Self
+}
 
-func deepCopy<T>(_ array: [T]) -> [T] {
-    return array.map(\.self)
+// 2. Extend basic value types
+extension Int: DeepCopyable {
+    func deepCopy() -> Self { self }
+}
+extension Float: DeepCopyable {
+    func deepCopy() -> Self { self }
+}
+extension Double: DeepCopyable {
+    func deepCopy() -> Self { self }
+}
+extension Bool: DeepCopyable {
+    func deepCopy() -> Self { self }
+}
+extension String: DeepCopyable {
+    func deepCopy() -> Self { self }
+}
+// Add other value types as needed (Character, etc.)
+
+// 3. Extend collections conditionally
+extension Array: DeepCopyable where Element: DeepCopyable {
+    func deepCopy() -> Array {
+        map { $0.deepCopy() }
+    }
+}
+
+extension Dictionary: DeepCopyable where Value: DeepCopyable {
+    func deepCopy() -> Dictionary {
+        reduce(into: Dictionary()) { result, pair in
+            result[pair.key] = pair.value.deepCopy()
+        }
+    }
+}
+
+extension Set: DeepCopyable where Element: DeepCopyable {
+    func deepCopy() -> Set {
+        Set(map { $0.deepCopy() })
+    }
+}
+
+// 4. Implement global deep copy function
+func deepCopy<T: DeepCopyable>(_ value: T) -> T {
+    value.deepCopy()
 }
 
