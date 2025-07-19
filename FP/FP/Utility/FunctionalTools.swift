35e0e53ae408100a95057d93084b4f2eceb09ca2
kmnemon <kmnemon@outlook.com>
2025-07-20 05:32:45 +0700

    recur

--- FP/FP/Utility/FunctionalTools.swift
+++ FP/FP/Utility/FunctionalTools.swift
@@ -14,8 +14,21 @@ func update<T: Indexable, R>(_ object: T, _ key: Field, _ modify: (R)->R) -> T {
     return newObject
 }
 
-func update2<T: Indexable, R>(_ object: T, _ key1: Field, _ key2: Field, _ modify: (R)->R) -> T {
+func update2(_ object: Indexable, _ key1: Field, _ key2: Field, _ modify: (Indexable)->Indexable) -> Indexable {
     return update(object, key1) { (value1: Indexable) in
         return update(value1, key2, modify)
+    }
+}
+
+func nestedUpdate(_ object: Indexable, _ keys: [Field], _ modify: (Indexable)->Indexable) -> Indexable {
+    guard !keys.isEmpty else {
+        return modify(object)
+    }
+    
+    let key1 = keys[0]
+    let restKeys = Array(keys[1...])
+    
+    return update(object, key1) { (value1: Indexable) in
+        return nestedUpdate(value1, restKeys, modify)
     }
 }
