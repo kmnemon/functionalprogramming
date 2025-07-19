6971f1705463d9f69531135e434be94b7df6db49
kmnemon <kmnemon@outlook.com>
2025-07-19 19:09:38 +0700

    update2

--- FP/FP/Utility/FunctionalTools.swift
+++ FP/FP/Utility/FunctionalTools.swift
@@ -5,10 +5,17 @@
 //  Created by ke Liu on 7/18/25.
 //
 
-func update<T: Indexable, R>(_ object: inout T, _ key: Field, _ modify: (R)->R) -> T {
-    let value = object[key] as! R
+func update<T: Indexable, R>(_ object: T, _ key: Field, _ modify: (R)->R) -> T {
+    var copy = object
+    let value = copy[key] as! R
     let newValue = modify(value)
     
-    let newObject = object.setValue(for: key, value: newValue)
+    let newObject = copy.setValue(for: key, value: newValue)
     return newObject
+}
+
+func update2<T: Indexable, R>(_ object: T, _ key1: Field, _ key2: Field, _ modify: (R)->R) -> T {
+    return update(object, key1) { (value1: Indexable) in
+        return update(value1, key2, modify)
+    }
 }
