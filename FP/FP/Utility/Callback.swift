c1b91701184146deaa456d339c2e8065a7b7ad5b
kmnemon <kmnemon@outlook.com>
2025-07-26 07:55:28 +0700

    callback

--- FP/FP/Utility/Callback.swift
+++ FP/FP/Utility/Callback.swift
@@ -5,27 +5,40 @@
 //  Created by ke Liu on 7/25/25.
 //
 
-func queue<T> (_ worker: @escaping (T, () -> Void) -> Void) -> (T) ->Void  {
-    var items: [T] = []
+struct Item<T, R> {
+    var data: T
+    var callback: (R) -> Void
+}
+
+func queue<T, R> (_ worker: @escaping (T, (R) -> Void) -> Void) -> (T, @escaping (R)->Void ) ->Void  {
+    var queueItems: [Item<T, R>] = []
+    var working = false
     
     func runNext() {
-        guard !items.isEmpty else { return }
-        let item = items.removeFirst()
+        if working { return }
+        if queueItems.isEmpty { return }
+        working = true
         
-        worker(item) {
+        guard !queueItems.isEmpty else { return }
+        let item = queueItems.removeFirst()
+        
+        worker(item.data) { val in
+            working = false
+            item.callback(val)
             runNext()
         }
     }
     
-    return { (_ data: T) in
-        items.append(data)
+    return { (_ data: T, _ callback: ((R)->Void)? ) in
+        queueItems.append(Item(data: data, callback: callback ?? { _ in} ))
         runNext()
     }
 }
 
-func calcWorker(_ item: Int, _ done: ()->Void) {
+func calcWorker(_ item: Int, _ done: (Int)->Void) {
     //calc bussiness
-    done()
+    var total = 100
+    done(total)
 }
 
 var updateTotalQueue = queue(calcWorker)
