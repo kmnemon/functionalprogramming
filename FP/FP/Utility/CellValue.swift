c2ae65cc2c324b7ee8aa40b81b18d46c11e2447e
kmnemon <kmnemon@outlook.com>
2025-07-30 10:31:16 +0700

    watcher

--- FP/FP/Utility/CellValue.swift
+++ FP/FP/Utility/CellValue.swift
@@ -5,24 +5,34 @@
 //  Created by ke on 7/30/25.
 //
 
-class Cell<T> {
-    private var currentValue: T
+class Cell<T: Equatable> {
+    private var currentValue: T?
+    private var watchers: [(T?)->Void] = []
     
-    init(currentValue: T) {
+    init(currentValue: T?) {
         self.currentValue = currentValue
     }
     
-    var value: T {
+    var value: T? {
         get { currentValue }
     }
     
-    func update(_ f: (T)->T) {
+    func update(_ f: (T?)->T?) {
         var oldValue = currentValue
         var newValue = f(oldValue)
-        currentValue = newValue
+        if oldValue != newValue {
+            currentValue = newValue
+            watchers.forEach { watcher in
+                watcher(newValue)
+            }
+        }
+    }
+    
+    func addWatcher(_ f: @escaping (T?)->Void) {
+        watchers.append(f)
     }
 }
 
-func valueCell<T>(_ initialValue: T) -> Cell<T> {
+func valueCell<T>(_ initialValue: T?) -> Cell<T> {
     return Cell(currentValue: initialValue)
 }
