b39c175051ee293e082102c970d7b966caaf7af7
kmnemon <kmnemon@outlook.com>
2025-07-31 08:09:05 +0700

    cellvalue

--- FP/FP/Utility/CellValue.swift
+++ FP/FP/Utility/CellValue.swift
@@ -6,20 +6,29 @@
 //
 
 class Cell<T: Equatable> {
-    private var currentValue: T?
-    private var watchers: [(T?)->Void] = []
+    private var currentValue: T
+    private var watchers: [(T)->Void] = []
     
-    init(currentValue: T?) {
-        self.currentValue = currentValue
+    init(val: T) {
+        self.currentValue = val
     }
     
-    var value: T? {
+    init(currentValue: T, watchers: [ (T) -> Void]) {
+        self.currentValue = currentValue
+        self.watchers = watchers
+    }
+    
+    var value: T {
         get { currentValue }
     }
     
-    func update(_ f: (T?)->T?) {
-        var oldValue = currentValue
-        var newValue = f(oldValue)
+    var ws: [(T)->Void] {
+        get { self.watchers }
+    }
+    
+    func update(_ f: (T)->T) {
+        let oldValue = currentValue
+        let newValue = f(oldValue)
         if oldValue != newValue {
             currentValue = newValue
             watchers.forEach { watcher in
@@ -28,11 +37,22 @@ class Cell<T: Equatable> {
         }
     }
     
-    func addWatcher(_ f: @escaping (T?)->Void) {
+    func addWatcher(_ f: @escaping (T)->Void) {
         watchers.append(f)
     }
 }
 
-func valueCell<T>(_ initialValue: T?) -> Cell<T> {
-    return Cell(currentValue: initialValue)
+func valueCell<T: Equatable>(_ initialValue: T) -> Cell<T> {
+    return Cell(val: initialValue)
+}
+
+func formulaCell<T: Equatable, R: Equatable>(_ upstreamCell: Cell<T>, _ f: @escaping (T)->R) -> Cell<R> {
+    var myCell = valueCell(f(upstreamCell.value))
+    upstreamCell.addWatcher { newUpstreamValue in
+        myCell.update { currentValue in
+            return f(newUpstreamValue)
+        }
+    }
+    
+    return Cell(currentValue: myCell.value, watchers: myCell.ws)
 }
